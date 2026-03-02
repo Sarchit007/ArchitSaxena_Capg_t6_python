6065298482251e369d9ad272182fa5640c7d3dc5
Sarchit007 <sarchit971@gmail.com>
2026-03-02 12:58:54 +0530

    adding new file named as Practice_Questions

---
+++ Practice_Questions.py
@@ -0,0 +1,14 @@
+units = int(input("Enter units consumed: "))
+
+bill = 0
+
+if units <= 100:
+    bill = units * 5
+
+elif units <= 300:
+    bill = (100 * 5) + (units - 100) * 7
+
+else:
+    bill = (100 * 5) + (200 * 7) + (units - 300) * 10
+
+print("Total Bill Amount:", bill)
